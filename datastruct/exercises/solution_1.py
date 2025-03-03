29b4fb5dfbd4b8d6b5d5a084033f41f6241c77b7
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2025-03-03 17:34:42 -0500

    correpcion de recorrido

--- datastruct/exercises/solution_1.py
+++ datastruct/exercises/solution_1.py
@@ -7,7 +7,7 @@ def add_two_numbers(l1: LinkedList[int], l2: LinkedList[int]) -> LinkedList[int]
 
     node1, node2 = l1.head, l2.head
 
-    while l1 or l2 or carry:
+    while node1 or node2 or carry:
         if l1:
             val1 = node1.data
         else:
