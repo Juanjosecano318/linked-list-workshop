d7e8c1aaaa3d4ecaaa9eef989f1d0e4303e1e372
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2025-03-03 17:14:30 -0500

    creamos lista y carry

--- datastruct/exercises/solution_1.py
+++ datastruct/exercises/solution_1.py
@@ -2,9 +2,16 @@ from datastruct.classes.lists import LinkedList
 
 
 def add_two_numbers(l1: LinkedList[int], l2: LinkedList[int]) -> LinkedList[int]:
-    # Implement solution here
-    dummy_head = LinkedList(0)
-    current = dummy_head
+    result = LinkedList[int]()
     carry = 0
+
+
+
+
+
+
+
+
+
 
 
