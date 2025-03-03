e2531f8abda3540d05826b11b670ab71ded04115
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2025-03-03 16:43:14 -0500

    Inicializar variables

--- datastruct/exercises/solution_1.py
+++ datastruct/exercises/solution_1.py
@@ -3,4 +3,8 @@ from datastruct.classes.lists import LinkedList
 
 def add_two_numbers(l1: LinkedList[int], l2: LinkedList[int]) -> LinkedList[int]:
     # Implement solution here
-    pass
+    dummy_head = LinkedList(0)
+    current = dummy_head
+    carry = 0
+
+
