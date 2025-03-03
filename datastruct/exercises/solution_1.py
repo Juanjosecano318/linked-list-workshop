1bfb35320b97e6e40f18e4555c1bc2ae3911e594
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2025-03-03 17:27:53 -0500

    asigno valor a nodos y creo el ciclo que recorre las listas

--- datastruct/exercises/solution_1.py
+++ datastruct/exercises/solution_1.py
@@ -5,6 +5,20 @@ def add_two_numbers(l1: LinkedList[int], l2: LinkedList[int]) -> LinkedList[int]
     result = LinkedList[int]()
     carry = 0
 
+    node1, node2 = l1.head, l2.head
+
+    while l1 or l2 or carry:
+        if l1:
+            val1 = node1.data
+        else:
+            val1 = 0
+        if l2:
+            val2 = node2.data
+        else:
+            val2 = 0
+
+
+
 
 
 
