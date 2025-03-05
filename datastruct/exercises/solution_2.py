ff909afc29b59165fe581b1abbb24a319a0743fb
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2025-03-05 14:43:21 -0500

    definicion de punteros e intercambio nodos

--- datastruct/exercises/solution_2.py
+++ datastruct/exercises/solution_2.py
@@ -3,12 +3,31 @@ from datastruct.classes.lists import LinkedList
 
 def swap_nodes_in_pairs(linked_list: LinkedList[int]) -> LinkedList[int]:
 
-    lista = linked_list.head
+    if not linked_list.head:
+        return linked_list
+    if not linked_list.head.next:
+        return linked_list
 
-    if lista is None:
-        return linked_list
-    if not lista.next:
-        return linked_list
+    head = linked_list.head
+
+    prev = None
+    first = head
+    second = head.next
+
+    while first and first.next:
+        second = first.next
+        first.next = second.next
+        if second:
+            second.next = first
+        if prev:
+            pass
+
+
+
+
+
+
+
 
 
 
