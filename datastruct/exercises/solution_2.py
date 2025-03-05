c207c3cf4e9a668e85f04ab32aa68d89503b9b2f
Juanjosecanog <juanjosecanogiraldo5@gmail.com>
2025-03-05 13:52:18 -0500

    casos bases

--- datastruct/exercises/solution_2.py
+++ datastruct/exercises/solution_2.py
@@ -2,5 +2,16 @@ from datastruct.classes.lists import LinkedList
 
 
 def swap_nodes_in_pairs(linked_list: LinkedList[int]) -> LinkedList[int]:
-    # Implement solution here
-    pass
+
+    lista = linked_list.head
+
+    if lista is None:
+        return linked_list
+    if not lista.next:
+        return linked_list
+
+
+
+
+
+
